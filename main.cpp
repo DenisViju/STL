2a14ba77d7a2211d40a8e8d6b984452ab6616d63
Denis Viju <denisviju24@gmail.com>
2024-11-26 17:19:24 +0200

    Bonus Curs2

--- main.cpp
+++ main.cpp
@@ -1,33 +1,81 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+struct Doctor {
+    string id;
+    string speciality;
+    int time;
+    vector <string> problemeRezolvate;
+};
+
+struct Problem {
+    string id;
+    string speciality;
+    int time;
+};
 
 int main()
 {
     ifstream inFile("input.txt");
 
-    int no_problems, no_doctors;
-    string name, speciality;
-    
-    inFile >> no_problems;
+    if (!inFile) {
+        cerr << "Nu s-a putut deschide fișierul input.txt\n";
+        return 1;
+    }
 
-    for (int i = 0; i < no_problems; i++)
-    {
+    int no_problems, no_doctors, time;
+    string name, speciality;
+    vector<Doctor> doctors;
+    vector<Problem> problems;
+
+    inFile >> no_problems;
+    problems.reserve(no_problems);
+
+    for (int i = 0; i < no_problems; i++) {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> time;
+        problems.push_back(Problem{ name, speciality, time });
+        
     }
 
     inFile >> no_doctors;
+    doctors.reserve(no_doctors);
 
-    for (int i = 0; i < no_doctors; i++)
-    {
+    for (int i = 0; i < no_doctors; i++) {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctors.push_back(Doctor{ name, speciality, 8, {}});
+
+
+    }
+    
+    for (auto& problem : problems) {
+       auto doctorIt = find_if(doctors.begin(), doctors.end(), [&problem](Doctor& doctor) {
+            return problem.speciality == doctor.speciality && doctor.time >= problem.time;
+            });
+       
+    
+        if (doctorIt != doctors.end()) {
+                doctorIt->problemeRezolvate.push_back(problem.id);
+                doctorIt->time -= problem.time;
+        }
+
+    }
+    for (auto& doctor : doctors) {
+        if (!doctor.problemeRezolvate.empty()) {
+            cout << doctor.id << " " << doctor.problemeRezolvate.size();
+            for (auto& problema : doctor.problemeRezolvate)
+                cout << " " << problema;
+            cout << endl;
+        }
     }
 
+    
     return 0;
 }
